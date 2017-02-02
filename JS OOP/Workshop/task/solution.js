5bd2c956517c727dd703d035fc4c43ee19a35456
Camyul <vankata.lt@outlook.com>
2017-02-02 11:15:19 +0200

    JS WorkShop Task - 8 errors

--- JS OOP/Workshop/task/solution.js
+++ JS OOP/Workshop/task/solution.js
@@ -1,14 +1,50 @@
 function solve() {
-	function getProduct(productType, name, price) {
-	}
+    function getProduct(productType, name, price) {
+        let product = {
+            productType: productType,
+            name: name,
+            price: price
+        }
 
-	function getShoppingCart() {
-	}
+        return product;
+    }
 
-	return {
-		getProduct: getProduct,
-		getShoppingCart: getShoppingCart
-	};
+    function getShoppingCart() {}
+
+    return {
+        getProduct: getProduct,
+        getShoppingCart: function getShoppingCart() {
+            let shopingCart = {
+                products: [],
+                add: function(args) {
+
+                    this.products.push(args);
+
+                    return this;
+                },
+                remove: function(args) {
+                    let index = this.products.indexOf(args);
+
+                    if (this.products.length === 0) {
+                        throw new Error('shopingCart is Empty');
+                    }
+                    if (index > -1) {
+                        this.products.splice(index, 1);
+                    } else {
+                        throw new Error('shopingCart don\'t contain this product');
+
+                    }
+
+                    return this;
+                },
+                showCost: function() {},
+                showProductTypes: function() {},
+                getInfo: function() {}
+            }
+
+            return shopingCart;
+        }
+    };
 }
 
 module.exports = solve();
