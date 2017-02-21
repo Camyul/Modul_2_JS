ee3399d2d51b619517c2081757c3a5926c03f315
Camyul <vankata.lt@outlook.com>
2017-02-22 01:32:12 +0200

    JS BattleManager - 8 errors

--- JS OOP/Workshop - Live Demo/10-September-2016/task-2/task/solution.js
+++ JS OOP/Workshop - Live Demo/10-September-2016/task-2/task/solution.js
@@ -38,23 +38,23 @@ function solve() {
 
     const Validator = {
         validateNameExist(str, name) {
-            if (str === null || typeof str === 'string') {
+            if (str === null || typeof str !== 'string') {
                 throw new Error(`${name} must be string!`);
             }
         },
         validateStringLength(str, name) {
             if (str.length < 2 || str.length > 20) {
-                throw new Error(`${name} length must be between 2 and 20!`);
+                throw new Error(`${name} must be between between 2 and 20 symbols long!`);
             }
         },
         validateOnlyLatin(str, name) {
-            if (str.match(/^A-Za-z /)) {
-                throw new Error(`${name} must contain only latin symbols and whitespaces!`);
+            if (str.match(/[^A-Za-z ]/)) {
+                throw new Error(`${name} can contain only latin symbols and whitespaces!`);
             }
         },
         validatePositiveInteger(num, name) {
             if (num < 1 || typeof num !== 'number' || num !== (num | 0)) {
-                throw new Error(`${name} must be positive integer number!`);
+                throw new Error(`${name} must be a positive integer number!`);
             }
         },
 
@@ -65,7 +65,7 @@ function solve() {
         },
 
         validatePositiveValue(num, name, maxValue) {
-            if (typeof num !== 'number' && (num <= 0 || num > maxValue)) {
+            if (typeof num !== 'number' && (num < 0 || num > maxValue)) {
                 throw new Error(`${name} must be a positive number that is at most ${maxValue}!`)
             }
         }
@@ -76,7 +76,7 @@ function solve() {
         constructor(name, manaCost, effect) {
             this.name = name;
             this.manaCost = manaCost;
-            this._effect = effect;
+            this.effect = effect;
         }
 
         get name() {
@@ -98,6 +98,19 @@ function solve() {
 
             this._manaCost = newMana;
         }
+
+        get effect() {
+            return this._effect;
+        }
+        set effect(effect) {
+            if (effect === null) {
+                throw new Error(`Passed objects must be Spell-like objects!`);
+            }
+
+            if (typeof effect !== 'function') {
+                throw new Error(`Effect must be a function with 1 parameter!`);
+            }
+        }
     }
 
     class Unit {
@@ -111,7 +124,7 @@ function solve() {
         }
         set name(name) {
             Validator.validateStringLength(name, 'Name');
-            Validator.validateStringLength(name, 'Name');
+            Validator.validateNameExist(name, 'Name');
             Validator.validateOnlyLatin(name, 'Name');
 
             this._name = name;
@@ -245,40 +258,77 @@ function solve() {
                 throw new Error(`No such commander`);
             }
 
-            spells.forEach(x => function(x) {
-                if (typeof x !== 'Spell') {
-                    throw new Error(`Passed objects must be Spell-like objects!`);
-                    return this;
-                }
-            });
 
             commander.spellbook.push(...spells);
             return this;
         }
 
         findCommanders(query) {
-            /* let commanders
-             if (opt.hasOwnProperty('name')) {
-                 commanders = this._commanders.filter(x => x.name === opt.name);
-                 //.every(x => x.alignment === opt.alignment);
-                 commanders.sort(x => x.name);
-             } else {
-                 commanders = this._commanders.filter(x => x.alignment === opt.alignment);
-                 commanders.sort(x => x.name);
-             }
 
-             if (commanders.length === 0) {
-                 throw new Error(`no matches, []`);
-             } else {
-                 return commanders;
-             }*/
-
+            //query is an object that can have properties name and/or alignment
+            //returns all commanders that have the same values of the passed properties
             return this._commanders
                 .filter(commander => Object.keys(query).every(prop => query[prop] === commander[prop]));
         }
 
         findArmyUnitById(id) {
+            let armyUnit = this._army_units.find(x => x.id === id);
 
+            return armyUnit;
+        }
+
+
+        //query is an object that can have properties id, name, alignment
+        //returns an array of all ArmyUnits in the Battlemanager that have the same values for the passed properties
+        findArmyUnits(query) {
+            return this._army_units
+                .filter(army => Object.keys(query).every(prop => query[prop] === army[prop]));
+        }
+
+        spellcast(casterName, spellName, targetUnitId) {
+            let commander = this._commanders.find(c => c.name === casterName);
+            if (commander === undefined) {
+                throw new Error(`Can\'t cast with non-existant commander ${casterName}!`)
+            };
+
+            let spell = commander.spellbook.find(s => s.name === spellName);
+            if (spell === undefined) {
+                throw new Error(`${casterName} does not know ${spellName}`)
+            }
+
+            if (commander.mana < spell.manaCost) {
+                throw new Error(`Not enough mana!`);
+            }
+
+            let army = this._army_units.find(a => a.id === targetUnitId);
+            if (army === undefined) {
+                throw new Error(`Target not found!`);
+            }
+
+            commander.mana -= spell.manaCost;
+
+            //spell.effect(targetUnitId);
+
+            return this;
+        }
+
+        battle(attacker, defender) {
+            if (attacker === null || defender === null) {
+                throw new Error(`Nee na men`);
+            }
+
+            let attackerTotalDamage = attacker.damage * attacker.count,
+                defenderTotalHealth = defender.health * defender.count;
+
+            defenderTotalHealth -= attackerTotalDamage;
+
+            defender.count = Math.ceil(defenderTotalHealth / defender.health);
+
+            if (defender.count < 0) {
+                defender.count = 0;
+            }
+
+            return this;
         }
     }
 
