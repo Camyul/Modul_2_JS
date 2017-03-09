5376157f3383e8ccc480c10058eaad89f271d211
Camyul <vankata.lt@outlook.com>
2017-03-10 00:04:24 +0200

    JS Workshop Work on task - One

--- JavaScript-UI-and-DOM/Workshop - Dom and Dom manipulations/items-control/task/tasks/task-1.js
+++ JavaScript-UI-and-DOM/Workshop - Dom and Dom manipulations/items-control/task/tasks/task-1.js
@@ -1,33 +1,37 @@
 function solve() {
+
     return function(selector, isCaseSensitive) {
-        var element = selector,
+        let element = selector,
             fragment,
 
             addControls,
             labelAdd,
             btnAdd,
-            ipAdd;
+            ipAdd,
+
+            searchControls,
+            labelSearch,
+            ipSearch;
 
         if (isCaseSensitive !== true) {
             isCaseSensitive = false;
         }
 
         if (typeof element === 'string') {
-            element = document.querySelector('element');
+            element = document.querySelector(element);
         }
 
         if (!element || !(element instanceof HTMLElement)) {
-            throw new Error('Invalid HTML Element or selector');
+            throw new Error("Invalid HTML element or selector");
         }
 
         fragment = document.createDocumentFragment();
 
-        //Add Controls - Start
+        /* Add Controls - Start */
         addControls = document.createElement('div');
         addControls.className = 'add-controls';
 
         labelAdd = document.createElement("label");
-        //labelAdd.innerText = 'Enter Text: ';
         labelAdd.innerHTML = 'Enter Text: ';
 
         ipAdd = document.createElement("input");
@@ -36,20 +40,49 @@ function solve() {
 
         btnAdd = document.createElement('a');
         btnAdd.className = 'button';
-        //btnAdd.addEventListener('click', onAddButtonClick, false);
+        btnAdd.addEventListener('click', onAddButtonClick, false);
         btnAdd.innerHTML = 'Add';
         btnAdd.style.display = 'inline-block';
 
         addControls.appendChild(labelAdd);
         addControls.appendChild(btnAdd);
-        //Add Controls END
+        /* Add Controls END */
 
+        /* Searching Controls START*/
+        searchControls = document.createElement('div');
+        searchControls.className = 'search-controls';
 
+        labelSearch = document.createElement('label');
+        labelSearch.innerHTML = 'searrch';
+
+        ipSearch = document.createElement('input');
+        ipSearch.addEventListener('click', onSearchTextboxInput, false);
+
+        labelSearch.appendChild(ipSearch);
+        searchControls.appendChild(labelSearch);
+
+        /* Searching Controls END*/
+
+        /* Result Controls START*/
+
+        /* Result Controls END*/
 
         fragment.appendChild(addControls);
+        fragment.appendChild(searchControls);
 
         element.appendChild(fragment);
         element.className += 'items-control';
+
+        function onAddButtonClick() {
+            let value = ipAdd.value;
+            ipAdd.value = '';
+
+
+        };
+
+        function onSearchTextboxInput() {
+
+        };
     };
 }
 
