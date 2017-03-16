ac0139e602b5e7ea64c7e177b6112a49e21c1ced
Camyul <vankata.lt@outlook.com>
2017-03-16 14:40:29 +0200

    JS jQuery Plugins Complete task-1

--- JavaScript-UI-and-DOM/06. jQuery Plugins/task-1/tasks/task-1.js
+++ JavaScript-UI-and-DOM/06. jQuery Plugins/task-1/tasks/task-1.js
@@ -1,20 +1,76 @@
 function solve() {
     return function(selector) {
+        var $body,
+            $element,
+            $selectContainer,
+            $selectedOption,
+            $optionContainer,
+            defaultOption,
+            $options = $(selector).find('option');
+
+
         if (typeof selector !== 'string' || $(selector).length < 1) {
             throw new Error('Wrong input');
         }
 
-        var $body = $(document.body),
-            $element = $(selector)
+        $body = $(document.body);
+
+        $element = $(selector)
             .attr('id', 'the-select')
             .css('display', 'none');
 
-        var $selectContainer = $('<div></div>')
+        $selectContainer = $('<div></div>')
             .addClass('dropdown-list')
             .appendTo($body);
 
-        $selectContainer.append($element);
+
+        $optionContainer = $('<div />')
+            .addClass('options-container')
+            .css('position', 'absolute')
+            .css('display', 'none')
+            .appendTo($selectContainer);
+
+        for (var i = 0, len = $options.length; i < len; i += 1) {
+            var $currentOption = $('<div />')
+                .addClass('dropdown-item')
+                .attr('data-value', `value-${i + 1}`)
+                .attr('data-index', `${i}`)
+                .css('color', 'red')
+                .html($options[i].text)
+                .appendTo($optionContainer);
+
+            if (i === 0) {
+                defaultOption = $currentOption;
+            }
+        }
+
+        $selectedOption = $('<div></div>')
+            .addClass('current')
+            .attr('data-value', '')
+            .html(defaultOption.text())
+            .prependTo($selectContainer);
+
+
+        $selectContainer.prepend($element);
+
+        $selectedOption.on('click', function() { //mouseover work to
+            var $this = $(this);
+
+            $optionContainer.css('display', '');
+
+            $this.html('Select Option');
+        });
+
+        $optionContainer.on('click', '.dropdown-item', function() {
+            var $this = $(this),
+                html = $this.html();
+
+            $selectedOption.html(html);
+
+            $optionContainer.css('display', 'none');
+        });
     };
 }
+
 
 module.exports = solve;
