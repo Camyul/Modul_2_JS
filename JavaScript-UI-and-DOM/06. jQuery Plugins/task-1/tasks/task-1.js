0faa0f7e1672ea9ef27623f3dccab0d8cddc6dd5
Camyul <vankata.lt@outlook.com>
2017-03-16 02:04:28 +0200

    JS jQuery Plugins Start task-1

--- JavaScript-UI-and-DOM/06. jQuery Plugins/task-1/tasks/task-1.js
+++ JavaScript-UI-and-DOM/06. jQuery Plugins/task-1/tasks/task-1.js
@@ -1,6 +1,19 @@
 function solve() {
     return function(selector) {
-        console.log($);
+        if (typeof selector !== 'string' || $(selector).length < 1) {
+            throw new Error('Wrong input');
+        }
+
+        var $body = $(document.body),
+            $element = $(selector)
+            .attr('id', 'the-select')
+            .css('display', 'none');
+
+        var $selectContainer = $('<div></div>')
+            .addClass('dropdown-list')
+            .appendTo($body);
+
+        $selectContainer.append($element);
     };
 }
 
