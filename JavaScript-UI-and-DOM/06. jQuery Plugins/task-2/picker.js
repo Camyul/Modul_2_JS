5b3a1d2d9a5eb7fc9712dcd95d6c6ade3fe0f7e9
Camyul <vankata.lt@outlook.com>
2017-03-18 23:53:09 +0200

    JS jQuery Plugins Task-2 Complete

--- JavaScript-UI-and-DOM/06. jQuery Plugins/task-2/picker.js
+++ JavaScript-UI-and-DOM/06. jQuery Plugins/task-2/picker.js
@@ -8,10 +8,13 @@ $.fn.colorpicker = function() {
         $inputRGB,
         $inputColor,
         canvas,
-        ctx;
+        ctx,
+        img;
 
     canvas = document.createElement('canvas');
     ctx = canvas.getContext('2d');
+
+
 
     $root.css('width', '42px')
         .css('height', '42px')
@@ -22,18 +25,25 @@ $.fn.colorpicker = function() {
 
     $colorPickerPanel = $('<div/>')
         .addClass('color-picker-panel')
-        .css('width', '100px')
-        .css('height', '300px')
-        .css('position', 'relative')
+        .css('width', '260px')
+        .css('height', '315px')
+        .css('position', 'absolute')
         .hide()
         .appendTo($root);
+
+    canvas.width = 256;
+    canvas.height = 256;
+    $colorPickerPanel.append(canvas);
+    canvas.setAttribute('id', 'the-canvas');
+
+
 
     $closeBtn = $('<img/>')
         .addClass('close-btn')
         .attr('src', './imgs/close.png')
         .css('position', 'absolute')
-        .css('top', '-45px')
-        .css('left', '-130px')
+        .css('top', '-30px')
+        .css('left', '-45px')
         .appendTo($colorPickerPanel)
         .click(function() {
             $colorPickerPanel.hide();
@@ -42,35 +52,35 @@ $.fn.colorpicker = function() {
             $startIcon.show();
         });
 
-    $colopPalette = $('<img/>')
-        .addClass('color-palette')
-        .attr('src', './imgs/color-picker.png')
-        .css('position', 'absolute')
-        .css('top', '-15px')
-        .css('left', '-80px')
-        .appendTo($colorPickerPanel);
+
+    img = document.createElement('img');
+    img.src = './imgs/color-picker.png';
+    img.id = 'canvas-image-container';
+    document.body.appendChild(img);
+
+    drawCanvas();
 
     $inputHEX = $('<input/>')
         .addClass('input-HEX')
         .css('position', 'absolute')
-        .css('top', '262px')
-        .css('left', '-97px')
+        .css('top', '282px')
+        .css('left', '-10px')
         .attr('placeholder', 'HEX')
         .appendTo($colorPickerPanel);
 
     $inputRGB = $('<input/>')
         .addClass('input-RGB')
         .css('position', 'absolute')
-        .css('top', '292px')
-        .css('left', '-97px')
+        .css('top', '312px')
+        .css('left', '-10px')
         .attr('placeholder', 'RGB')
         .appendTo($colorPickerPanel);
 
     $inputColor = $('<input/>')
         .addClass('input-color')
         .css('position', 'absolute')
-        .css('top', '262px')
-        .css('left', '80px')
+        .css('top', '282px')
+        .css('left', '150px')
         .appendTo($colorPickerPanel);
 
     $startIcon = $('<img/>')
@@ -85,7 +95,21 @@ $.fn.colorpicker = function() {
             $colorPickerPanel.show();
         });
 
+    function drawCanvas() {
+        ctx.drawImage(img, 0, 0, 256, 256);
+    }
 
 
 
+    canvas.addEventListener('mousemove', function(ev) {
+        let x = ev.layerX,
+            y = ev.layerY,
+            pixel = ctx.getImageData(x, y, 1, 1),
+            rgb = `rgb( ${pixel.data[0]}, ${pixel.data[1]}, ${pixel.data[2]})`,
+            hexColor = `#${pixel.data[0].toString(16).toUpperCase()}${pixel.data[1].toString(16).toUpperCase()}${pixel.data[2].toString(16).toUpperCase()}`;
+
+        $inputColor.css('background', `${rgb}`);
+        $inputRGB.val(rgb);
+        $inputHEX.val(hexColor);
+    });
 }
