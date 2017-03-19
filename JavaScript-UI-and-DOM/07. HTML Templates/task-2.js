902c0fa84b02efc981af520b6fb8934253475579
Camyul <vankata.lt@outlook.com>
2017-03-19 19:26:54 +0200

    JS Handlebars Templates Task-2 Complete

--- JavaScript-UI-and-DOM/07. HTML Templates/task-2.js
+++ JavaScript-UI-and-DOM/07. HTML Templates/task-2.js
@@ -1,22 +1,32 @@
 /* globals $ */
 
 function solve() {
-  
-  return function (selector) {
-    var template = ''; /* insert the template here as a string
-        example:
-        var template =
-          '<ul>' +
-            '{{#students}}' +
-            '<li>' +
-              '{{name}}' +
-            '</li>' +
-            '{{/students}}' +
-          '</ul>';
-    */
 
-    $(selector).html(template);
-  };
+    return function(selector) {
+        var data = {
+            animals: [{
+                name: 'Lion',
+                url: 'https://susanmcmovies.files.wordpress.com/2014/12/the-lion-king-wallpaper-the-lion-king-2-simbas-pride-4685023-1024-768.jpg'
+            }, {
+                name: 'Turtle',
+                url: 'http://www.enkivillage.com/s/upload/images/a231e4349b9e3f28c740d802d4565eaf.jpg'
+            }, {
+                name: 'Dog'
+            }, {
+                name: 'Cat',
+                url: 'http://i.imgur.com/Ruuef.jpg'
+            }, {
+                name: 'Dog Again'
+            }]
+        }
+
+        var hbTemplate = document.getElementById('hb-template-container').innerHTML;
+
+
+        template = Handlebars.compile(hbTemplate);
+
+        $(selector).html(template(data));
+    };
 };
 
 module.exports = solve;
