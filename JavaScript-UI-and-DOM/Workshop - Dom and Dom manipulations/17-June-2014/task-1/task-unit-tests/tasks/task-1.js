0146f1216b46ca23dee3c4dd8f3ef874d0c3bb5c
Camyul <vankata.lt@outlook.com>
2017-03-25 20:34:24 +0200

    JS Workshop Not Completed

--- JavaScript-UI-and-DOM/Workshop - Dom and Dom manipulations/17-June-2014/task-1/task-unit-tests/tasks/task-1.js
+++ JavaScript-UI-and-DOM/Workshop - Dom and Dom manipulations/17-June-2014/task-1/task-unit-tests/tasks/task-1.js
@@ -1,73 +1,120 @@
 /* globals module */
 function solve() {
 
-  function clear(node) {
-    while (node.firstChild) {
-      node.removeChild(node.firstChild);
+    // empty the container
+    function clear(node) {
+        while (node.firstChild) {
+            node.removeChild(node.firstChild);
+        }
     }
-  }
 
-  return function (selector, items) {
-    var root = document.querySelector(selector);
-    var left = document.createElement('div');
-    left.className = 'image-preview';
-    var title = document.createElement('strong');
-    title.innerHTML = items[0].title;
-    var img = document.createElement('img');
-    img.src = items[0].url;
-    left.appendChild(title);
-    left.appendChild(img);
+    return function(selector, items) {
+        let root = document.querySelector(selector),
+            leftDiv = document.createElement('div'),
+            rightDiv = leftDiv.cloneNode(true),
+            img,
+            leftStrong,
+            leftHeather,
+            rightStrong,
+            rightHeather,
+            filter,
+            input,
+            innerRightDiv;
 
-    var right = document.createElement('div');
+        leftDiv.className += 'left';
+        img = document.createElement('img');
+        img.src = items[0].url;
+        img.className = 'left-container';
+        leftStrong = document.createElement('strong');
+        leftHeather = document.createElement('h2');
+        leftHeather.innerText = items[0].title;
+        leftStrong.appendChild(leftHeather);
+        leftDiv.appendChild(leftStrong);
+        leftDiv.appendChild(img);
 
-    items.forEach(function (item) {
-      var imgContainer = document.createElement('div');
-      imgContainer.className = 'image-container';
+        rightDiv.className += 'right';
+        filter = document.createElement('h4');
+        filter.innerHTML = 'Filter';
+        input = document.createElement('input');
 
-      var title = document.createElement('strong');
-      title.innerHTML = item.title;
-      var img = document.createElement('img');
-      img.width = '150';
-      img.src = item.url;
+        rightDiv.appendChild(filter);
+        rightDiv.appendChild(input);
 
-      imgContainer.appendChild(title);
-      imgContainer.appendChild(img);
 
-      right.appendChild(imgContainer);
-    });
+        for (let i = 0, len = items.length; i < len; i += 1) {
+            let tempImage = document.createElement('img'),
+                imgContainer = document.createElement('ul'),
+                liTamplate = document.createElement('li');
 
-    right.addEventListener('click', function (ev) {
-      var target = ev.target;
-      if (!(target instanceof HTMLImageElement)) {
-        return;
-      }
-      img.src = target.src;
-      title.innerHTML = target.previousElementSibling.innerHTML;
-    });
+            rightStrong = document.createElement('strong');
+            rightHeather = document.createElement('h4');
+            rightHeather.innerHTML = items[i].title;
+            rightStrong.appendChild(rightHeather);
+            liTamplate.appendChild(rightStrong);
+            tempImage.src = items[i].url;
+            tempImage.alt = items[i].title;
+            liTamplate.appendChild(tempImage);
+            liTamplate.className += 'image-container';
 
-    right.addEventListener('mouseover', function (ev) {
-      var target = ev.target;
-      console.log(target.className);
-      if (target.className.indexOf('image-container') < 0) {
-        return;
-      }
-      target.style.background = 'black';
-    });
+            imgContainer.appendChild(liTamplate);
+            rightDiv.appendChild(imgContainer);
 
-    right.addEventListener('mouseout', function (ev) {
-      var target = ev.target;
+            tempImage.addEventListener('mouseover', function(ev) {
+                let target = ev.target;
+                if (target.parentElement.className.indexOf('image-container') < 0) {
+                    return;
+                }
 
-      if (target.className.indexOf('image-container') < 0) {
-        return;
-      }
+                target.parentElement.style.background = 'grey';
+            }, false);
 
-      target.style.background = '';
-    });
+            tempImage.addEventListener('mouseout', function(ev) {
+                let target = ev.target;
+                if (target.parentElement.className.indexOf('image-container') < 0) {
+                    return;
+                }
 
-    clear(root);
-    root.appendChild(left);
-    root.appendChild(right);
-  };
+                target.parentElement.style.background = '';
+            }, false);
+
+            tempImage.addEventListener('click', function(ev) {
+                let target = ev.target;
+
+                if (!(target instanceof HTMLElement)) {
+                    return;
+                }
+
+                img.src = target.src;
+                leftHeather.innerText = target.alt;
+            }, false);
+
+        }
+
+        input.addEventListener('keyup', function(ev) {
+            let searchedValue = this.value.toLowerCase(),
+                liItems = document.querySelectorAll('li');
+            //console.log(searchedValue);
+
+            for (let i = 0, len = liItems.length; i < len; i += 1) {
+                let nameImage = liItems[i].firstChild.alt;
+                console.log(nameImage);
+                console.log(nameImage);
+                if (nameImage.indexOf(searchedValue) < 0) {
+                    liItems[i].style.display = 'none';
+                } else {
+                    liItems[i].style.display = '';
+                }
+            }
+
+        }, false);
+
+
+
+
+        clear(root);
+        root.appendChild(leftDiv);
+        root.appendChild(rightDiv);
+    };
 }
 
 module.exports = solve;
