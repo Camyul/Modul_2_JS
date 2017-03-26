339157217f1e82a97a3bea7e8526e9ed26236a55
Camyul <vankata.lt@outlook.com>
2017-03-26 23:52:24 +0300

    JS Task-2 Item-Control Start Work

--- JavaScript-UI-and-DOM/Workshop - Dom and Dom manipulations/6-August-2015/task-1/tasks/task-1.js
+++ JavaScript-UI-and-DOM/Workshop - Dom and Dom manipulations/6-August-2015/task-1/tasks/task-1.js
@@ -1,6 +1,53 @@
 function solve() {
-  return function (selector, isCaseSensitive) {
-  };
+    return function(selector, isCaseSensitive) {
+        let root = selector,
+            addContainer,
+            addLabel,
+            searchLabel,
+            inputAdd,
+            inputSearch,
+            btnAdd,
+            searchContainer;
+
+        if (typeof root === "string") {
+            root = document.querySelector(root);
+        }
+        if (!root || !(root instanceof HTMLElement)) {
+            throw new Error('Missing selector');
+        }
+
+        addContainer = document.createElement('div');
+        addContainer.className += 'items-control';
+
+        addLabel = document.createElement('label');
+        addLabel.innerHTML = 'Enter text';
+        inputAdd = document.createElement('input');
+        addLabel.appendChild(inputAdd);
+
+        btnAdd = document.createElement('a');
+        btnAdd.className += 'button';
+        btnAdd.innerHTML = 'Add';
+        btnAdd.style.display = 'inline-block';
+
+        addContainer.appendChild(addLabel);
+        addContainer.appendChild(btnAdd);
+
+        root.appendChild(addContainer);
+
+
+        searchContainer = document.createElement('div');
+        searchContainer.className += 'search-controls';
+
+        searchLabel = document.createElement('label');
+        searchLabel.innerHTML = 'Search:';
+
+        inputSearch = document.createElement('input');
+        searchLabel.appendChild(inputSearch);
+
+        searchContainer.appendChild(searchLabel);
+
+        root.appendChild(searchContainer);
+    };
 }
 
 module.exports = solve;
