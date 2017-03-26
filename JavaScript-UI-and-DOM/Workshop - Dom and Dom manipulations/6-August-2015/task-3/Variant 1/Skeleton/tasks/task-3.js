ae7bad3956a947d77a6735dac866981c8a6d8e04
Camyul <vankata.lt@outlook.com>
2017-03-26 20:29:02 +0300

    JS Exam 08.2015 Task-3 Completed

--- JavaScript-UI-and-DOM/Workshop - Dom and Dom manipulations/6-August-2015/task-3/Variant 1/Skeleton/tasks/task-3.js
+++ JavaScript-UI-and-DOM/Workshop - Dom and Dom manipulations/6-August-2015/task-3/Variant 1/Skeleton/tasks/task-3.js
@@ -1,6 +1,25 @@
 function solve() {
-    return function (selector) {
-        var template = '';		
+    return function(selector) {
+        var template = '<h2 class="header">Appointments for <span class="month">{{month}}</span> <span class="year">{{year}}</span></h2>' +
+            '{{#days}}' +
+            '<div class="col-date">' +
+            '<div class="date">{{day}}</div>' +
+            '<div class="events">' +
+            '{{#each events}} {{#if title}}' +
+            '<div class="event {{importance}}" title="{{comment}}">' +
+            '<div class="title">' +
+            '{{title}}' +
+            '</div>' +
+            '<div class="time">' +
+            'at {{time}}' +
+            '</div>' +
+            '</div>' +
+            '{{else}}' +
+            '<div class="title">Free slot</div>' +
+            '{{/if}} {{/each}}' +
+            '</div>' +
+            '</div>' +
+            '{{/days}}';
         document.getElementById(selector).innerHTML = template;
     };
 }
