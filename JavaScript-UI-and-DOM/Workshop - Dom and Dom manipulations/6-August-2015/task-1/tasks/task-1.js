1bd421d247f8d2299ab2db6f1fe6263841b386d0
Camyul <vankata.lt@outlook.com>
2017-03-27 12:17:28 +0300

    JS Task-1 Items Control Complete

--- JavaScript-UI-and-DOM/Workshop - Dom and Dom manipulations/6-August-2015/task-1/tasks/task-1.js
+++ JavaScript-UI-and-DOM/Workshop - Dom and Dom manipulations/6-August-2015/task-1/tasks/task-1.js
@@ -7,7 +7,14 @@ function solve() {
             inputAdd,
             inputSearch,
             btnAdd,
-            searchContainer;
+            searchContainer,
+            resultContainer,
+            itemsContainer,
+            newAnchor,
+            newLi,
+            anchorTamplate,
+            liTamplate,
+            strongElement;
 
         if (typeof root === "string") {
             root = document.querySelector(root);
@@ -16,8 +23,10 @@ function solve() {
             throw new Error('Missing selector');
         }
 
+        root.className += 'items-control';
+
         addContainer = document.createElement('div');
-        addContainer.className += 'items-control';
+        addContainer.className += 'add-controls';
 
         addLabel = document.createElement('label');
         addLabel.innerHTML = 'Enter text';
@@ -29,6 +38,8 @@ function solve() {
         btnAdd.innerHTML = 'Add';
         btnAdd.style.display = 'inline-block';
 
+        btnAdd.addEventListener('click', addCreateElement, false);
+
         addContainer.appendChild(addLabel);
         addContainer.appendChild(btnAdd);
 
@@ -36,17 +47,86 @@ function solve() {
 
 
         searchContainer = document.createElement('div');
-        searchContainer.className += 'search-controls';
+        searchContainer.className = 'search-controls';
 
         searchLabel = document.createElement('label');
         searchLabel.innerHTML = 'Search:';
 
         inputSearch = document.createElement('input');
+        inputSearch.addEventListener('keyup', filterElement, false);
+
         searchLabel.appendChild(inputSearch);
 
         searchContainer.appendChild(searchLabel);
 
         root.appendChild(searchContainer);
+
+
+        resultContainer = document.createElement('div');
+        resultContainer.className = 'result-controls';
+
+        itemsContainer = document.createElement('ul');
+        itemsContainer.className += 'items-list';
+        resultContainer.appendChild(itemsContainer);
+
+        itemsContainer.addEventListener('click', deleteButtonClick, false);
+
+
+        anchorTamplate = document.createElement('a');
+        anchorTamplate.className += 'button';
+        anchorTamplate.innerHTML = 'X';
+
+        strongElement = document.createElement('strong');
+
+        liTamplate = document.createElement('li');
+        liTamplate.className += 'list-item';
+        liTamplate.appendChild(anchorTamplate);
+        liTamplate.appendChild(strongElement);
+
+
+
+        root.appendChild(resultContainer);
+
+
+        function filterElement(ev) {
+            let value = inputSearch.value,
+                items = document.querySelectorAll('.list-item');
+
+            if (!isCaseSensitive) {
+                value = value.toLowerCase();
+            }
+
+            for (let i = 0, len = items.length; i < len; i += 1) {
+                let text = items[i].children[1].innerText;
+
+                if (!isCaseSensitive) {
+                    text = text.toLowerCase();
+                }
+
+                if (text.indexOf(value) < 0) {
+                    items[i].style.display = 'none';
+                } else {
+                    items[i].style.display = '';
+                }
+            }
+        };
+
+        function deleteButtonClick(ev) {
+            let node = ev.target;
+
+            if (node.className === 'button') {
+                itemsContainer.removeChild(node.parentNode);
+            }
+        };
+
+        function addCreateElement() {
+            let newLi = liTamplate.cloneNode(true);
+
+            newLi.children[1].innerHTML = inputAdd.value;
+            inputAdd.value = '';
+
+            itemsContainer.appendChild(newLi);
+        };
     };
 }
 
