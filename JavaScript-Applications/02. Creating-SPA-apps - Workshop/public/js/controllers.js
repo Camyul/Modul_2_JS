f3879ed94345d986a60e8650cb09435939ae72f0
Camyul <vankata.lt@outlook.com>
2017-04-18 20:39:31 +0300

    JS SPA-apps Workshop add templates

--- JavaScript-Applications/02. Creating-SPA-apps - Workshop/public/js/controllers.js
+++ JavaScript-Applications/02. Creating-SPA-apps - Workshop/public/js/controllers.js
@@ -6,7 +6,28 @@ let controllers = {
             home() {
                 var cookies;
                 dataService.cookies()
-                    .then()
+                    .then((cookiesResponse) => {
+                        cookies = cookiesResponse;
+                        console.log(cookies);
+
+                        return templates.get('home');
+                    })
+                    .then((templateHtml) => {
+                        let templateFunk = handlebars.compile(templateHtml),
+                            html = templateFunk(cookies);
+
+                        $('#container').html(html);
+
+                        $('.btn-like-dislike').on('click', function(ev) {
+                            let type = $('this').attr('data-type'),
+                                cookieId = $('this').parents('li').attr('data-id');
+
+                            console.log(type);
+                            console.log(cookieId);
+                            dataService.rateCookie(cookieId, type)
+                                .then() //Someting...
+                        });
+                    });
             }
         };
     }
